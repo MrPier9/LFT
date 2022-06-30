aa1c3dd749192c8af9abaf8193d695ccd7ce6de3
Pierluigi Secco <pierluigi.secco@edu.unito.it>
2022-06-30 17:48:12 +0200

    NumberTok uodated

--- LabLFT/NumberTok.java
+++ LabLFT/NumberTok.java
@@ -1,10 +1,29 @@
 public class NumberTok extends Token {
-	public int lexeme;
-	public NumberTok(int n) {
-		super(Tag.NUM); 
+	public static int lexeme;
+
+	// public NumberTok(int tag, int n) {
+	// super(tag);
+	// lexeme = n;
+	// }
+
+	public NumberTok(int tag, int n) {
+		super(tag);
 		lexeme = n;
 	}
-	
-	public String toString() {return "<" + Tag.NUM + "," + lexeme + ">";}
+
+	public String toString() {
+		return "<" + tag + "," + lexeme + ">";
+	}
+
+	public static int getVal() {
+		return lexeme;
+	}
+
+	public static void setVal(int n) {
+		lexeme = n;
+	}
+
+	public static final NumberTok num = new NumberTok(Tag.NUM, lexeme);
+
+	// public final NumberTok num = new NumberTok(Tag.NUM, lexeme);
 }
-	
